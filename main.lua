efab60ef50089d4c29f3085d59c26c0fed863537
avani-1660 <47641293+avani-1660@users.noreply.github.com>
2019-04-24 19:53:53 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+local Button = display.newRect( 160, 400, 60, 30)
+
+
+
+local textBox = native.newTextField( 160, 140, 200, 25)
+textBox.id = "TextBox"
+
+local answerText = display.newText( "", 160, 300, "Times New Roman", 25)
+answerText:setFillColor( 1, 1, 1)
+
+local Check function Checker( event ) 
+
+math.randomseed( 1234)
+
+local GuessNumber = math.random( 1, 12)
+
+print (GuessNumber)
+
+local answer = tonumber(textBox.text)
+
+	if (answer == GuessNumber) then
+		answerText.text = "You guessed it!"
+	else
+		answerText.text = "Try again!"
+
+	end
+
+	return true
+end
+
+Button:addEventListener( "touch", Checker ) 
